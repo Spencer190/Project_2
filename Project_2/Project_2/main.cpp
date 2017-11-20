6f86568ba3f7c4ab2f1c406115c9321d51f42695
Spencer Kranz <skranz190@github.com>
2017-11-20 15:40:29 -0500

    jfkld;fj

---
+++ Project_2/Project_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project_2
+//
+//  Created by Spencer Kranz on 11/20/17.
+//  Copyright © 2017 Spencer Kranz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, crazy World!\n";
+    return 0;
+}
